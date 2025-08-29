f75178fdbfa37bb33b946f18f2d1c305e1aa377a
Zhang Y.D. <me@cloumy.top>
2025-08-29 12:56:35 -0400

    Initial Commit

---
+++ src/main/java/vaniercollege/zyd/Essay.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vaniercollege.zyd;
+
+/**
+ *
+ * @author 2480549
+ */
+public class Essay {
+    
+}
---
+++ src/main/java/vaniercollege/zyd/GradedActivity.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vaniercollege.zyd;
+
+/**
+ *
+ * @author 2480549
+ */
+public class StudentManage {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
