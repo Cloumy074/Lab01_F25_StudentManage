5e6f0013a8fee873855248e7a3e7e9ef81978a5d
Zhang Y.D. <me@cloumy.top>
2025-08-29 23:17:40 -0400

    Step 4: Essay (Done & Note that test file seems not using the right data)

--- src/main/java/vaniercollege/zyd/Essay.java
+++ src/main/java/vaniercollege/zyd/Essay.java
@@ -1,5 +1,7 @@
 package vaniercollege.zyd;
 
+import java.util.Scanner;
+
 /**
  *
  * @author 2480549
@@ -9,25 +11,51 @@ public class Essay extends GradedActivity {
     private double spelling;
     private double correctLength;
     private double content;
+    Scanner sc = new Scanner(System.in);
 
-    @Override
     public void setScore(double grammar, double spelling, double correctLength, double content) {
-        super.setScore(grammar + spelling + correctLength + content);
+        setGrammar(grammar);
+        setSpelling(spelling);
+        setCorrectLength(correctLength);
+        setContent(content);
+
+        super.setScore(this.getGrammar() + this.getSpelling() + this.getCorrectLength() + this.getContent());
+        super.getGrade();
     }
 
     public void setGrammar(double grammar) {
+        while (grammar > 30) {
+            System.out.println("ERROR: grammar must be less than 30\nGrammaer points: ");
+            grammar = sc.nextDouble();
+        }
+
         this.grammar = grammar;
     }
 
     public void setSpelling(double spelling) {
+        while (spelling > 20) {
+            System.out.println("ERROR: spelling must be less than 20\nSpelling points: ");
+            spelling = sc.nextDouble();
+        }
+
         this.spelling = spelling;
     }
 
     public void setCorrectLength(double correctLength) {
+        while (correctLength > 20) {
+            System.out.println("ERROR: CorrectLength must be less than 20\nCorrect Length points: ");
+            correctLength = sc.nextDouble();
+        }
+
         this.correctLength = correctLength;
     }
 
     public void setContent(double content) {
+        while (content > 30) {
+            System.out.println("ERROR: content must be less than 30\nContent points: ");
+            content = sc.nextDouble();
+        }
+
         this.content = content;
     }
 
@@ -45,5 +73,10 @@ public class Essay extends GradedActivity {
 
     public double getContent() {
         return content;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Total points: %.1f\nGrade: %c", this.getScore(), this.getGrade());
     }
 }
